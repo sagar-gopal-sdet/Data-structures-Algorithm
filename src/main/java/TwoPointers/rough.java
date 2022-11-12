4f7fe1a3a3c65dd0a2d78cb510b139de8bf18b6c
Sagar Gopal <sagar1091@gmail.com>
2022-11-12 09:29:58 +0530

    Solved Hashmap problems

--- src/main/java/TwoPointers/rough.java
+++ src/main/java/TwoPointers/rough.java
@@ -3,13 +3,14 @@ package TwoPointers;
 import java.util.Arrays;
 
 public class rough {
+    /*Class file just to verify some one line logics without impacting the framework*/
     public static void main(String args[]){
 
 
         String s = "I'm Sagar";
         String s1="Gopal";
         String[] s3 = (s+" "+s1).split(" ");
-        System.out.println(Arrays.toString(s3));
+
     }
 
 
