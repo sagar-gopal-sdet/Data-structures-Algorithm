dcc440c98c33d6e520880304fc950a762f8c19b9
Sagar Gopal <sagar1091@gmail.com>
2022-11-27 15:03:31 +0530

    Students and Sandwich problem solved

--- src/main/java/General/SandwichAndStudents.java
+++ src/main/java/General/SandwichAndStudents.java
@@ -1,2 +1,42 @@
-package General;public class SandwichAndStudents {
+package General;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SandwichAndStudents {
+
+    @Test
+    public void td1(){
+        Assert.assertEquals(findStudents(new int[] {1,1,1,0,0,1}, new int[] {1,0,0,0,1,1}), 3);
+    }
+
+    /*Pseudocode:
+    * */
+
+    public int findStudents(int[] students, int[] sandwiches){
+        Deque<Integer> stu = new ArrayDeque<>();
+        for(int i=0;i<students.length;i++){
+            stu.add(students[i]);
+        }
+        int i = 0,temp=0;
+        while(temp < stu.size()){
+            if(stu.peek() == sandwiches[i]){
+                i++;
+                temp = 0;
+                stu.poll();
+            } else {
+                stu.add(stu.poll());
+                temp++;
+            }
+        }
+
+
+
+        return stu.size();
+    }
 }
