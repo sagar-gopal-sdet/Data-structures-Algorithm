cac85b48df7160531e5e84ec7648569d4110ff68
Sagar Gopal <sagar1091@gmail.com>
2022-11-29 15:43:08 +0530

    Lambda

--- src/main/java/InterviewQuestions/TraverseMapUsingLambda.java
+++ src/main/java/InterviewQuestions/TraverseMapUsingLambda.java
@@ -1,2 +1,22 @@
-package InterviewQuestions;public class TraverseMapUsingLambda {
+package InterviewQuestions;
+
+import jdk.dynalink.beans.StaticClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TraverseMapUsingLambda {
+
+
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(1,1);
+        map.put(2,2);
+        map.put(3,3);
+        map.forEach((k,v)->{
+            System.out.println(k+v);
+        });
+    }
 }
