da18d69e410b3c1f828ee710e148e6c6a9a69c3b
Sagar Gopal <sagar1091@gmail.com>
2022-12-02 18:21:09 +0530

    Minesweeper Pseudocode

---
+++ src/main/java/Code1.java
@@ -0,0 +1,16 @@
+public class Code1 {
+
+    /*Pseudocode
+    * Initialize 2 lists- List<List<Integer>> mainListand List<Integer> subList
+    * Initialize for loop with range i=0 to x
+    * Initiate an inner loop with range j=i to y
+    * Initialize flag = 0
+    * Initiate a while loop with condition flag = 0
+    * Generate a random number within the limit of m
+    * Considering the the random number as box location, verify the box is empty.
+    * If empty, add 1 and break;
+    *  */
+
+}
+
+//
