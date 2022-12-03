a1ad41a3128c3bcc4782c1ac9fb19b7e250667cc
Sagar Gopal <sagar1091@gmail.com>
2022-12-03 10:13:13 +0530

    Find index problem solved using Binary search

---
+++ src/main/java/BinarySearch/FindIndex.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FindIndex {
+    @Test
+    public void td1(){
+        Assert.assertEquals(findIndexUsingBinarySearch(new int[] {-1,0,3,5,9,12},9),4);
+    }
+
+    public int findIndexUsingTwoPointers(int[] arr, int n){
+        int i=0,j=arr.length-1;
+        while(i<j){
+            if(arr[i]==n) return i;
+            if(arr[j]==n) return j;
+            i++;
+            j--;
+        }
+        return -1;
+    }
+
+    public int findIndexUsingBinarySearch(int[] arr, int n){
+        int left = 0, right=arr.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(arr[mid]<n) left=mid+1;
+            else if(arr[mid]>n) right=mid-1;
+            else if (arr[mid]==n) return mid;
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/SlidingWindow/NiceString.java
@@ -0,0 +1,18 @@
+package SlidingWindow;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NiceString {
+
+    @Test
+    public void td1(){
+        Assert.assertEquals(findNiceString("YazaAay"),"aAa");
+    }
+
+    public String findNiceString(String s){
+        String out = "";
+
+        return out;
+    }
+}
