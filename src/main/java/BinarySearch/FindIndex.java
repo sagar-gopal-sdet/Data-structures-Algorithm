43239c284ede3bbe51a9b6215a51da9c2f273c4c
Sagar Gopal <sagar1091@gmail.com>
2022-12-04 10:36:17 +0530

    Find Index problem solved

--- src/main/java/BinarySearch/FindIndex.java
+++ src/main/java/BinarySearch/FindIndex.java
@@ -9,6 +9,26 @@ public class FindIndex {
         Assert.assertEquals(findIndexUsingBinarySearch(new int[] {-1,0,3,5,9,12},9),4);
     }
 
+    @Test
+    public void td2(){
+        Assert.assertEquals(findIndexUsingBinarySearch(new int[] {-1,0,3,5,9,12},11),4);
+    }
+
+    @Test
+    public void td3(){
+        Assert.assertEquals(findIndexUsingBinarySearch(new int[] {1,3,5,7},6),3);
+    }
+
+    @Test
+    public void td4(){
+        Assert.assertEquals(findIndexUsingBinarySearch(new int[] {1,3,5,6},7),4);
+    }
+
+    @Test
+    public void td5(){
+        Assert.assertEquals(findIndexUsingBinarySearch(new int[] {1,3,5,6},0),0);
+    }
+
     public int findIndexUsingTwoPointers(int[] arr, int n){
         int i=0,j=arr.length-1;
         while(i<j){
@@ -28,6 +48,6 @@ public class FindIndex {
             else if(arr[mid]>n) right=mid-1;
             else if (arr[mid]==n) return mid;
         }
-        return -1;
+        return left-1;
     }
 }
