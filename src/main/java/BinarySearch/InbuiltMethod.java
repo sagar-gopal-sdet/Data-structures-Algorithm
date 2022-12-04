34f56ddd68f63f5d614c40f6d5d2c2d2c6b3fad2
Sagar Gopal <sagar1091@gmail.com>
2022-12-04 10:38:03 +0530

    Binary Search inbuilt method

--- src/main/java/BinarySearch/InbuiltMethod.java
+++ src/main/java/BinarySearch/InbuiltMethod.java
@@ -1,5 +1,29 @@
 package BinarySearch;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
 public class InbuiltMethod {
-    
+    @Test
+    public void td1(){
+        Assert.assertEquals(findPosition(new int[] {1,3,5,7},6),3);
+    }
+
+    @Test
+    public void td2(){
+        Assert.assertEquals(findPosition(new int[] {1,3,5,6},7),4);
+    }
+
+    @Test
+    public void td3(){
+        Assert.assertEquals(findPosition(new int[] {1,3,5,6},0),0);
+    }
+
+    public int findPosition(int[] nums, int target){
+        int index=Arrays.binarySearch(nums,target);
+        if(index<0) index = (index*-1)-1;
+        return index;
+    }
 }
