f718ec1d82a1005cf1b967bfb24e4f3be0238162
Sagar Gopal <sagar1091@gmail.com>
2022-12-05 14:42:26 +0530

    Recursion problems solved

--- src/main/java/Recursion/Fibonacci.java
+++ src/main/java/Recursion/Fibonacci.java
@@ -1,4 +1,30 @@
 package Recursion;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Fibonacci {
+    @Test
+    public void td1(){
+        Assert.assertEquals(fib(3), 2);
+    }
+
+
+
+    public int fib(int n){
+        if(n<=1) return n;
+        else return fib(n-1)+fib(n-2);
+    }
+
+    public int fibBruteForce(int n){
+        int a=0,b=1;
+        for(int i=1;i<n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+
+
 }
--- src/main/java/Recursion/ReverseString.java
+++ src/main/java/Recursion/ReverseString.java
@@ -6,11 +6,20 @@ import org.testng.annotations.Test;
 public class ReverseString {
     @Test
     public void td1(){
-        Assert.assertEquals(stringReverseUsingRecursion(new char[] {"h","e","l","l","o"}), new char[] {"o","l","l","e","h"});
+        Assert.assertEquals(stringReverse(new char[] {'h','e','l','l','o'}), new char[] {'o','l','l','e','h'});
     }
 
-    public char[] stringReverseUsingRecursion(char[] s){
-                
+    public char[] stringReverse(char[] s){
+        reverseUsingRecursion(0, s.length-1, s);
         return s;
+    }
+    public void reverseUsingRecursion(int start, int end, char[] c){
+        if(start<end){
+            char temp = c[start];
+            c[start++] = c[end];
+            c[end--]= temp;
+            reverseUsingRecursion(start,end,c);
+        }
+
     }
 }
