9efdae1a527c43271a34aa86ba675206cf5a12f6
Sagar Gopal <sagar1091@gmail.com>
2022-12-12 11:31:37 +0530

    Bubble sort algorithm

--- src/main/java/Sorting/BubbleSort.java
+++ src/main/java/Sorting/BubbleSort.java
@@ -1,2 +1,36 @@
-package Sorting;public class BubbleSort {
+package Sorting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    @Test
+    public void td1(){
+        Assert.assertEquals(bubbleSortArray(new int[] {1,5,4,6,7,3,8}), new int[] {1,3,4,5,6,7,8});
+    }
+
+    @Test
+    public void td2(){
+        Assert.assertEquals(bubbleSortArray(new int[] {1,9,10,-2,21,0,-809}), new int[] {-809,-2,0,1,9,10,21});
+    }
+
+    public int[] bubbleSortArray(int[] nums){
+        int i=0,j=nums.length-1;
+        while(j>0){
+            i=0;
+            while(i<j){
+                if(nums[i]>nums[i+1]){
+                    int temp = nums[i];
+                    nums[i]=nums[i+1];
+                    nums[i+1]=temp;
+                }
+                i++;
+            }
+            j--;
+        }
+        System.out.println(Arrays.toString(nums));
+        return nums;
+    }
 }
