c7daf8146033daf4f3368a7bd9d39b9e311245cd
Sagar Gopal <sagar1091@gmail.com>
2022-12-12 11:30:31 +0530

    Height checker problem solved

--- src/main/java/General/HeightChecker.java
+++ src/main/java/General/HeightChecker.java
@@ -1,2 +1,24 @@
-package General;public class HeightChecker {
+package General;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class HeightChecker {
+    @Test
+    public void td1(){
+        Assert.assertEquals(checkHeight(new int[] {1,1,4,2,1,3}), 3);
+    }
+
+    public int checkHeight(int[] heights){
+        int[] arr = heights.clone();
+        int sum = 0;
+        Arrays.sort(arr);
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=heights[i]) sum+=1;
+        }
+        return sum;
+    }
 }
