50d7d8544120805f0203d32b6cffa830faf43ea9
Sagar Gopal <sagar1091@gmail.com>
2022-12-13 10:56:30 +0530

    Merge interval

--- src/main/java/Sorting/MergeIntervals.java
+++ src/main/java/Sorting/MergeIntervals.java
@@ -1,2 +1,16 @@
-package Sorting;public class MergeIntervals {
+package Sorting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MergeIntervals {
+    @Test
+    public void td1(){
+        Assert.assertEquals(mergeIntervals(new int[][] {{1,3},{2,6},{8,10},{15,18}}),new int[][] {{1,6},{8,10},{15,18}});
+    }
+
+    public int[][] mergeIntervals(int[][] intervals){
+
+        return intervals;
+    }
 }
