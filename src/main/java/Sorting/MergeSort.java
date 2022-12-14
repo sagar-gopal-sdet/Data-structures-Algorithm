eaad797739f9a41e904e8de3be05731c4dcae4ca
Sagar Gopal <sagar1091@gmail.com>
2022-12-14 14:32:08 +0530

    MergeSort problem

--- src/main/java/Sorting/MergeSort.java
+++ src/main/java/Sorting/MergeSort.java
@@ -1,2 +1,14 @@
-package Sorting;public class MergeSort {
+package Sorting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MergeSort {
+   // @Test
+   /* public void td1(){
+        Assert.assertEquals(sort(new int[] {}), new int[] {});
+    }*/
+
+
+
 }
