34be757f84234950f693b59b4ffac4c2fb6217ea
Sagar Gopal <sagar1091@gmail.com>
2022-12-19 12:43:38 +0530

    Modified

--- src/main/java/TwoPointers/MergeSortedArrays.java
+++ src/main/java/TwoPointers/MergeSortedArrays.java
@@ -4,18 +4,8 @@ public class MergeSortedArrays {
     public int[] merge(int[] nums1, int m, int[] nums2, int n){
 
         int i=0, j=0;
-        while(i<m && j<n){
-            if(nums1[i]>nums2[j]){
-                int temp = nums1[i];
-                nums2[i]=nums1[j];
-                nums1[j]=temp;
-                i++;
-            } else {
-                j++;
-            }
-            while(j<(nums1.length+nums2.length)){
+        while(i<m+n){
 
-            }
         }
         return nums1;
     }
--- src/main/java/TwoPointers/rough.java
+++ src/main/java/TwoPointers/rough.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class rough {
     /*Class file just to verify some one line logics without impacting the framework*/
     public static void main(String args[]){
+        char ch = 'b';
+        char ch1 = 'B';
 
-
-        System.out.println(Math.abs(8/2));
+        System.out.println(ch1-ch);
 
 
     }
