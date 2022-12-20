19789ea6539cf3030c42c3f399ab4f077ed61077
Sagar Gopal <sagar1091@gmail.com>
2022-12-20 14:30:55 +0530

    Modifed

--- src/main/java/TwoPointers/rough.java
+++ src/main/java/TwoPointers/rough.java
@@ -1,5 +1,6 @@
 package TwoPointers;
 
+import javax.security.auth.callback.CallbackHandler;
 import java.util.Arrays;
 
 public class rough {
@@ -7,8 +8,12 @@ public class rough {
     public static void main(String args[]){
         char ch = 'b';
         char ch1 = 'B';
+        String s = "asdfghjkl";
+        s=s.replace('a','u');
+        StringBuilder sb = new StringBuilder("Sagar");
 
-        System.out.println(ch1-ch);
+        sb.setCharAt(0, Character.toUpperCase('u'));
+        System.out.println(sb.toString());
 
 
     }
