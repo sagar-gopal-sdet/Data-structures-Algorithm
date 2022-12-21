8cfdc651cbfd37df43d93aadcdece50d9839d19a
Sagar Gopal <sagar1091@gmail.com>
2022-12-21 15:23:03 +0530

    Rotate number

--- src/main/java/General/RotateNumber.java
+++ src/main/java/General/RotateNumber.java
@@ -1,2 +1,17 @@
-package General;public class RotateNumber {
+package General;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RotateNumber {
+    @Test
+    public void td1(){
+        Assert.assertEquals(rotateNumbersKTimes(1234,2),3421);
+    }
+
+    public int rotateNumbersKTimes(int n, int k){
+        int size = Integer.toString(n).length(), temp = size-1;
+
+        return 0;
+    }
 }
