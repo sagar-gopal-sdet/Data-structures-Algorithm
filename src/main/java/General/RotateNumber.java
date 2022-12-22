5e1f6bd8cd0d4cf002c94025f42871770c3d5dd9
Sagar Gopal <sagar1091@gmail.com>
2022-12-22 23:51:11 +0530

    RotateNumberCode

--- src/main/java/General/RotateNumber.java
+++ src/main/java/General/RotateNumber.java
@@ -3,6 +3,10 @@ package General;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
 public class RotateNumber {
     @Test
     public void td1(){
@@ -10,8 +14,20 @@ public class RotateNumber {
     }
 
     public int rotateNumbersKTimes(int n, int k){
-        int size = Integer.toString(n).length(), temp = size-1;
+        int size = Integer.toString(n).length(), temp = n;
+        ArrayList<Integer> al = new ArrayList<>();
 
+        while(temp>0){
+        int reminder=temp%10;
+        al.add(0,reminder);
+        temp=temp/10;
+        }
+        for (int i=0;i<k;i++){
+            al.add(al.get(0));
+            al.remove(0);
+        }
+        
+        System.out.println(al);
         return 0;
     }
 }
